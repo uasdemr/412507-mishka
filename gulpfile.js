e8c15580cfd3b644e9e3bf11a6f5ece372808ad2
uasdemr <sdyuzhilin@mail.ru>
2018-01-27 19:48:42 +0300

    Исправления третьему модулю. Подчистил код от комментариев и вернул настройки gulp

--- gulpfile.js
+++ gulpfile.js
@@ -20,7 +20,7 @@ gulp.task("style", function() {
 
 gulp.task("serve", ["style"], function() {
   server.init({
-    server: "",
+    server: "source/",
     notify: false,
     open: true,
     cors: true,
@@ -29,5 +29,4 @@ gulp.task("serve", ["style"], function() {
 
   gulp.watch("source/sass/**/*.{scss,sass}", ["style"]);
   gulp.watch("source/*.html").on("change", server.reload);
-  gulp.watch("*.html").on("change", server.reload);
 });
